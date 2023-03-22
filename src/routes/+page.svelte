6b9199e4fd02c80872b46c51c84352ec35f8b3b6
Siddhant Khare <siddhant@gitpod.io>
2023-03-22 11:22:53 +0000

    Dummy Opengraph Text along with its component

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import OpenGraph from "$lib/components/library/open-graph.svelte";
+</script>
+
+<OpenGraph
+  data={{
+    // TODO: #6 Update Opengraph data.
+    title: "CDE Universe - DevX Conf 2023",
+    description:
+      "The first conference dedicated to Cloud Development Environments (CDEs) and Developer Experience (DevX).",
+    keywords:
+      "cde, cloud development environment, devx, developer experience, devops, devops tools, devx tools, devx conference",
+  }}
+/>
