30d0f1dff8551bbb8d7e6e6c01e16e32138b28c8
GHOST <ghostdevbusiness@gmail.com>
2023-03-28 21:05:38 +0000

    fix: card

--- src/lib/components/library/cfp-card.svelte
+++ src/lib/components/library/cfp-card.svelte
@@ -3,14 +3,18 @@
 </script>
 
 <div
-  class="bg-blue rounded-xl shadow-lg py-20 w-full mx-auto relative overflow-clip z-[-2]"
+  class="bg-blue rounded-xl shadow-lg py-20 px-8 w-full mx-auto relative overflow-clip z-[-2]"
 >
-  <div class="mx-auto w-fit flex flex-col gap-12 items-start">
-    <h1 class="text-h1 font-medium">CFP is open</h1>
+  <div
+    class="mx-auto w-fit flex flex-col gap-9 items-center sm:items-start text-center sm:text-left"
+  >
+    <h1 class="text-6xl font-medium">CFP is open</h1>
 
     <p class="text-p-x-large max-w-md">
-      Interested in speaking onsite at the event? Our CFP is open until xx.yy.
+      Interested in speaking at CDE Universe?
     </p>
+
+    <p class="text-p-x-large max-w-md m-0">Submit a proposal until MM/DD/YY</p>
 
     <PrimaryButton text="Submit Proposal" />
   </div>
@@ -19,6 +23,6 @@
     src="/images/ring.webp"
     alt="ring"
     draggable={false}
-    class="absolute top-0 left-0 rotate-[-22deg] z-[-1] select-none scale-125"
+    class="absolute top-0 left-0 rotate-[-22deg] z-[-1] min-w-[800px] select-none scale-125"
   />
 </div>
