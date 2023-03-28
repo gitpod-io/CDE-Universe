1a236e925689ca35886369b50e0a13c6e7f68bb8
Siddhant Khare <siddhant@gitpod.io>
2023-03-28 08:57:35 +0000

    new navbar & new color scheme

---
+++ src/lib/components/svgs/cde-universe.svelte
@@ -0,0 +1,19 @@
+<script>
+  import CDEUniverseLogo from "$lib/components/svgs/cde-universe-logo.svelte";
+</script>
+
+<a
+  href="/"
+  class="flex flex-wrap w-full max-w-[248px] no-underline items-center"
+>
+  <CDEUniverseLogo class="mr-2 h-20 w-20 top-4 mt-2" />
+  <div class="flex flex-col mt-2">
+    <span class="text-3xl font-bold text-important">CDE</span>
+    <span class="text-3xl font-light text-important">universe</span>
+  </div>
+  <!-- <span class="text-3xl font-bold text-important">CDE</span> -->
+  <!-- <span class="text-3xl text-important">&nbsp;Conf</span> -->
+  <!-- <span class="text-xs font-bold ml-8 text-body">universe</span> -->
+  <!-- Suggestion: Include Gitpod logo instead of text:"Gitpod" -->
+  <!-- <Gitpod class="w-12 md:w-14" /> -->
+</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import Nav from "$lib/components/layout/nav.svelte";
   import Container from "$lib/components/layout/container.svelte";
   import Footer from "$lib/components/layout/footer.svelte";
   import "../app.css";
 </script>
 
 <div class="flex min-h-screen flex-col">
+  <Nav />
   <Container element="main" class="pt-xx-small sm:pt-x-small md:pt-small">
     <slot />
   </Container>
