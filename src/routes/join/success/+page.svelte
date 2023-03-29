d020e8adcfda82a737ef48ad226ffcf7b4d1ff12
GHOST <ghostdevbusiness@gmail.com>
2023-03-29 17:23:13 +0000

    fix: formatting

--- src/routes/join/success/+page.svelte
+++ src/routes/join/success/+page.svelte
@@ -1,5 +1,6 @@
 <div class="flex flex-col gap-8 items-center text-center">
   <h1 class="text-8xl">You're signed up</h1>
+
   <p class="text-xl max-w-lg">
     We'll get back to you shortly via email to let you know if you got one of
     the limited tickets.
