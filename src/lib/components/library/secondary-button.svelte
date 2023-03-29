d25dcdf74572218fba643dd10ab5afde63386fb5
GHOST <ghostdevbusiness@gmail.com>
2023-03-29 17:29:38 +0000

    fix: button styles

--- src/lib/components/library/secondary-button.svelte
+++ src/lib/components/library/secondary-button.svelte
@@ -3,23 +3,23 @@
   const target = href.startsWith("http") ? "_blank" : null;
   export let rel = "";
   export let text = "";
-  export let isDisabled = false;
+  export let disabled = false;
   export let element: "a" | "button" = "a";
+  export let style: "fill" | "line" = "fill";
   let className = "";
   export { className as class };
 </script>
 
 <svelte:element
   this={element}
-  class="rounded-xl no-underline px-5 py-1.5 text-sm font-bold {isDisabled
-    ? 'bg-bg btn-hover-border outline hover:outline-4'
-    : 'text-bg bg-headlines hover:bg-rose'} 
-}   {className}"
-  class:disabled={`${isDisabled} opacity-50 cursor-not-allowed`}
+  class="rounded-xl no-underline px-4 py-1 text-sm font-bold border-2 hover:border-rose disabled:opacity-50 disabled:cursor-not-allowed
+  {style == 'fill' ? 'bg-headlines hover:bg-rose text-bg' : ''} {className}"
+  {disabled}
   {target}
   {href}
   {rel}
   {...$$restProps}
+  on:click
 >
   {@html text}
 </svelte:element>
