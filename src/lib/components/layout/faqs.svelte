e07c955d8706cbc3e29b2b26a6a8a3c9f4389c87
GHOST <ghostdevbusiness@gmail.com>
2023-03-29 02:15:48 +0000

    feat: faqs

--- src/lib/components/layout/faqs.svelte
+++ src/lib/components/layout/faqs.svelte
@@ -7,7 +7,7 @@
 <div class="flex flex-col items-center space-y-8">
   <h1 class="font-bold text-4xl">FAQs</h1>
 
-  <div class="flex flex-wrap justify-between gap-16">
+  <div class="grid gap-x-8 gap-y-10 grid-flow-row-dense grid-cols-2">
     {#each faqs as faq}
       <div class="max-w-sm">
         <h4 class="font-bold text-xl">{faq.title}</h4>
---
+++ src/routes/waitlist/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Waitlist from "$lib/components/layout/waitlist.svelte";
+  import Faqs from "$lib/components/layout/faqs.svelte";
+  import { faqs } from "$lib/data";
+</script>
+
+<div class="max-w-5xl mx-auto space-y-36 pt-16 pb-36">
+  <Waitlist />
+  <Faqs {faqs} />
+</div>
