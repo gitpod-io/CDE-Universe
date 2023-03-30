57ee0d9ac2491cf34966dcd9fb36074a9d92d6ea
Siddhant Khare <siddhant@gitpod.io>
2023-03-30 17:10:45 +0000

    Header Ring :rocket:

--- src/lib/components/index/hero.svelte
+++ src/lib/components/index/hero.svelte
@@ -1,4 +1,6 @@
-<div class="mt-x-large w-11/12 mx-auto text-left md:text-center">
+<div
+  class="mt-x-large relative w-11/12 md:w-fit mx-auto justify-between text-left md:text-left md:mb-[400px]"
+>
   <h1 class="text-6xl font-extrabold">
     CDE <span class="font-light">universe</span>
   </h1>
@@ -7,4 +9,10 @@
     <div>In-Person, Pebblebed</div>
     <div>San Francisco, CA</div>
   </div>
+  <img
+    src="/images/primary-ellipse.webp"
+    alt="CDE Ellipse"
+    draggable={false}
+    class="max-w-4xl -z-10 select-none w-[85vw] transfrom scale-[2.12] absolute translate-x-[100%] translate-y-[-180%] rotate-[-6deg] md:transform md:translate-x-0 md:translate-y-[-80%] md:scale-150"
+  />
 </div>
