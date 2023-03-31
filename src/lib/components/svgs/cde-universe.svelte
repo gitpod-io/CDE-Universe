a14199eec6984a1017934d68738e4fecaf50b1b8
Siddhant Khare <siddhant@gitpod.io>
2023-03-31 09:33:44 +0000

    nit fix for mobile navbar

--- src/lib/components/svgs/cde-universe.svelte
+++ src/lib/components/svgs/cde-universe.svelte
@@ -2,5 +2,9 @@
   href="/"
   class="flex flex-wrap w-full max-w-[248px]  no-underline items-center"
 >
-  <img src="/navbar-logo.webp" alt="CDE Universe logo" class="w-9/12" />
+  <img
+    src="/navbar-logo.webp"
+    alt="CDE Universe logo"
+    class="w-1/2 md:w-9/12"
+  />
 </a>
