d8ced8c28ffaf7faa90f6f85e23a7018982d8b92
Siddhant Khare <siddhant@gitpod.io>
2023-03-31 12:02:36 +0000

    CFP card & FAQ Section cleanup

--- src/lib/components/layout/cfp-card.svelte
+++ src/lib/components/layout/cfp-card.svelte
@@ -7,17 +7,15 @@
     class="bg-blue rounded-xl shadow-lg py-20 px-8 w-full mx-auto relative overflow-clip z-0"
   >
     <div
-      class="mx-auto w-fit flex flex-col gap-9 items-start md:items-center text-left md:text-center"
+      class="mx-auto w-fit flex flex-col gap-9 items-start md:items-start text-left"
     >
       <h1 class="text-5xl md:text-6xl font-normal md:font-medium">
         CFP is open
       </h1>
 
-      <p class="text-p-large md:text-p-x-large max-w-md">
+      <p class="text-p-large md:text-p-x-large max-w-md text-left">
         Interested in speaking at CDE Universe?
-      </p>
-
-      <p class="text-p-large md:text-p-x-large max-w-md m-0">
+        <br /><br />
         Submit a proposal until April 30, 2023
       </p>
 
