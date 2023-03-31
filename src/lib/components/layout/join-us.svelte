994d1cd23e4dcf038dbbd90b86eef5b17f01bd5f
Siddhant Khare <siddhant@gitpod.io>
2023-03-31 17:34:47 +0530

    text alignment on all viewports (#68)

--- src/lib/components/layout/join-us.svelte
+++ src/lib/components/layout/join-us.svelte
@@ -7,25 +7,24 @@
   <div class="flex flex-col gap-20 items-start md:items-center">
     <div class="relative overflow-visible">
       <h1
-        class="font-bold text-headlines text-8xl text-center md:text-left md:text-[200px] lg:text-[280px] xl:text-[380px]"
+        class="font-bold text-headlines text-center md:text-left text-8xl md:text-[200px] xl:text-[380px]"
       >
-        Join Us
+        Join us
       </h1>
 
       <img
         style="top: 50%; left: 50%; transform: translate(-50%, -65%)"
-        class="absolute w-screen min-w-[100vw] z-[-10] "
+        class="absolute w-screen min-w-[110vw] z-[-10] "
         src="/images/ring-line.webp"
         alt="ring line"
       />
     </div>
 
     <div
-      class="grid-rows-2 grid text-left md:grid-rows-1 md:grid-cols-2 gap-8 mx-auto max-w-4xl"
+      class="grid-rows-2 grid text-left md:grid-rows-1 md:grid-cols-2 gap-12 md:gap-[120px] mx-auto max-w-4xl"
     >
       <div class="space-y-2">
-        <h4 class="font-bold text-2xl">Who?</h4>
-
+        <h4 class="font-bold text-[20px] md:text-[28px] mb-2 md:mb-4">Who?</h4>
         <p class="text-2xl">
           Meet the visionaries, builders and practitioners who make up the
           future of software development.
@@ -36,9 +35,8 @@
           engineering, DevOps, and developer experience.
         </p>
       </div>
-
       <div class="space-y-2">
-        <h4 class="font-bold text-2xl">Why?</h4>
+        <h4 class="font-bold text-[20px] md:text-[28px] mb-2 md:mb-4">Why?</h4>
 
         <p class="text-2xl">
           CDE Universe is small and curated. Connect with leading minds across
