19e4fd089981df8cf1f85cfe6ad03d54b40fec84
Siddhant Khare <siddhant@gitpod.io>
2023-03-31 15:04:46 +0530

    Navbar-improvements (#66)

--- src/lib/components/svgs/cde-universe.svelte
+++ src/lib/components/svgs/cde-universe.svelte
@@ -1,6 +1,10 @@
 <a
   href="/"
-  class="flex flex-wrap w-full max-w-[248px] no-underline items-center"
+  class="flex flex-wrap w-full max-w-[248px]  no-underline items-center"
 >
-  <img src="/navbar-logo.webp" alt="CDE Universe logo" />
+  <img
+    src="/navbar-logo.webp"
+    alt="CDE Universe logo"
+    class="w-1/2 md:w-9/12"
+  />
 </a>
