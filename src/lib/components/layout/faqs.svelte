0db9b3056b8455fbec424c8877393dc25093b8c3
Siddhant Khare <siddhant@gitpod.io>
2023-04-03 05:45:16 +0000

    minor fix on spacing in FAQ

--- src/lib/components/layout/faqs.svelte
+++ src/lib/components/layout/faqs.svelte
@@ -7,10 +7,12 @@
 <div class="flex flex-col items-center space-y-8">
   <h1 class="font-bold text-4xl">FAQs</h1>
 
-  <div class="grid gap-x-8 gap-y-10 grid-flow-row-dense grid-cols-2">
+  <div
+    class="grid gap-x-12 md:gap-x-[120px] gap-y-12 grid-flow-row-dense grid-cols-2"
+  >
     {#each faqs as faq}
       <div class="max-w-sm">
-        <h4 class="font-bold text-xl">{faq.title}</h4>
+        <h4 class="font-bold text-xl mb-2 md:mb-4">{faq.title}</h4>
         <p class="text-lg">{faq.description}</p>
       </div>
     {/each}
