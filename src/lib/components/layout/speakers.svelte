6ee5daca40f195b9ba63a33a7037c78ba0069ec9
Siddhant Khare <siddhant@gitpod.io>
2023-05-14 08:10:03 +0530

    :sparkles: Code Refactor for speakers section (#132)

--- src/lib/components/layout/speakers.svelte
+++ src/lib/components/layout/speakers.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import type { Speaker } from "$lib/types/speakers";
+  import type { PrimarySpeaker } from "$lib/types/primary-speakers";
   import Linkedin from "../svgs/linkedin.svelte";
   import Mastodon from "../svgs/mastodon.svelte";
   import Twitter from "../svgs/twitter.svelte";
   import GitHub from "../svgs/github.svelte";
 
   export let speakers: Speaker[];
+  export let primarySpeakers: PrimarySpeaker[];
 </script>
 
 <div
@@ -28,33 +30,33 @@
     <div
       class="flex gap-x-20 gap-y-12 flex-wrap justify-center md:justify-start max-w-6xl mx-auto items-stretch"
     >
-      {#each speakers as speaker}
-        <div class="w-56 mb-4" class:w-[325px]={speaker.large}>
+      {#each primarySpeakers as primarySpeaker}
+        <div class="mb-4 w-[325px]">
           <div
-            class="rounded-xl bg-blue border-blue border-8 mb-3 spkrsqr relative"
-            class:border-rose={speaker.large}
-            class:bg-rose={speaker.large}
+            class="rounded-xl bg-rose border-rose border-8 mb-3 spkrsqr relative"
           >
             <img
               class="rounded-xl image"
               width="100%"
               height="100%"
-              src={speaker.image}
-              alt="{speaker.name}'s picture"
+              src={primarySpeaker.image}
+              alt="{primarySpeaker.name}'s picture"
             />
             <div class="middle w-44">
-              <div class="text">{@html speaker.imgTitle || speaker.title}</div>
+              <div class="text">
+                {@html primarySpeaker.imgTitle || primarySpeaker.title}
+              </div>
             </div>
           </div>
 
           <div class="ml-2">
-            <h4 class="font-bold text-xl">{speaker.name}</h4>
-            <p>{@html speaker.title}</p>
+            <h4 class="font-bold text-xl">{primarySpeaker.name}</h4>
+            <p>{@html primarySpeaker.title}</p>
 
             <div class="flex gap-3 items-center mt-2">
-              {#if speaker.twitter}
+              {#if primarySpeaker.twitter}
                 <a
-                  href="https://twitter.com/{speaker.twitter}"
+                  href="https://twitter.com/{primarySpeaker.twitter}"
                   target="_blank"
                   class="social-link"
                 >
@@ -62,21 +64,29 @@
                 </a>
               {/if}
 
-              {#if speaker.mastodon}
-                <a href={speaker.mastodon} target="_blank" class="social-link">
+              {#if primarySpeaker.mastodon}
+                <a
+                  href={primarySpeaker.mastodon}
+                  target="_blank"
+                  class="social-link"
+                >
                   <Mastodon class="fill-[#EF9D83] fill:bg-[#ffc0be]" />
                 </a>
               {/if}
 
-              {#if speaker.linkedin}
-                <a href={speaker.linkedin} target="_blank" class="social-link">
+              {#if primarySpeaker.linkedin}
+                <a
+                  href={primarySpeaker.linkedin}
+                  target="_blank"
+                  class="social-link"
+                >
                   <Linkedin class="fill-[#EF9D83] hover:fill-[#ffc0be]" />
                 </a>
               {/if}
 
-              {#if speaker.github}
+              {#if primarySpeaker.github}
                 <a
-                  href="https://github.com/{speaker.github}"
+                  href="https://github.com/{primarySpeaker.github}"
                   target="_blank"
                   class="social-link"
                 >
@@ -84,6 +94,84 @@
                 </a>
               {/if}
             </div>
+          </div>
+        </div>
+      {/each}
+    </div>
+
+    <div
+      class="flex gap-x-20 gap-y-12 flex-wrap justify-center md:justify-start max-w-6xl mx-auto items-stretch"
+    >
+      {#each speakers as speaker}
+        <div class="w-56 mb-4">
+          {#if !speaker.large}
+            <div
+              class="rounded-xl bg-blue border-blue border-8 mb-3 spkrsqr relative"
+              class:border-rose={speaker.large}
+              class:bg-rose={speaker.large}
+            >
+              <img
+                class="rounded-xl image"
+                width="100%"
+                height="100%"
+                src={speaker.image}
+                alt="{speaker.name}'s picture"
+              />
+              <div class="middle w-44">
+                <div class="text">
+                  {@html speaker.imgTitle || speaker.title}
+                </div>
+              </div>
+            </div>
+          {/if}
+
+          <div class="ml-2">
+            {#if !speaker.large}
+              <h4 class="font-bold text-xl">{speaker.name}</h4>
+              <p>{@html speaker.title}</p>
+
+              <div class="flex gap-3 items-center mt-2">
+                {#if speaker.twitter}
+                  <a
+                    href="https://twitter.com/{speaker.twitter}"
+                    target="_blank"
+                    class="social-link"
+                  >
+                    <Twitter class="fill-[#EF9D83] hover:fill-[#ffc0be]" />
+                  </a>
+                {/if}
+
+                {#if speaker.mastodon}
+                  <a
+                    href={speaker.mastodon}
+                    target="_blank"
+                    class="social-link"
+                  >
+                    <Mastodon class="fill-[#EF9D83] fill:bg-[#ffc0be]" />
+                  </a>
+                {/if}
+
+                {#if speaker.linkedin}
+                  <a
+                    href={speaker.linkedin}
+                    target="_blank"
+                    class="social-link"
+                  >
+                    <Linkedin class="fill-[#EF9D83] hover:fill-[#ffc0be]" />
+                  </a>
+                {/if}
+
+                {#if speaker.github}
+                  <a
+                    href="https://github.com/{speaker.github}"
+                    target="_blank"
+                    class="social-link"
+                  >
+                    <GitHub class="fill-[#EF9D83] hover:fill-[#ffc0be]" />
+                  </a>
+                {/if}
+              </div>
+            {/if}
           </div>
         </div>
       {/each}
