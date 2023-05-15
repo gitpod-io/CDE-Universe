722902615190b3d11c7eda2f808fa8c5a71f7192
Siddhant Khare <siddhant@gitpod.io>
2023-05-15 18:23:41 +0530

    :sparkles: CDE Universe Ellipse (#134)

---
+++ src/lib/components/svgs/ellipse.svelte
@@ -0,0 +1,142 @@
+<script lang="ts">
+  let clazz = "";
+  export { clazz as class };
+</script>
+
+<svg
+  width="1686"
+  height="1223"
+  viewBox="0 0 1686 1223"
+  fill="none"
+  class={clazz}
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+>
+  <title>CDE Universe Ellipse</title>
+  <g filter="url(#filter0_ddii_118_3)">
+    <path
+      d="M1536.57 404.532C1566.56 516.437 1526.19 643.275 1421.82 759.973C1317.83 876.237 1154.64 976.501 956.078 1029.71C757.513 1082.91 566.055 1077.68 417.87 1028.98C269.133 980.102 170.753 890.441 140.769 778.537C110.784 666.632 151.153 539.794 255.524 423.097C359.508 306.832 522.698 206.568 721.263 153.363C919.828 100.158 1111.29 105.393 1259.47 154.09C1408.21 202.967 1506.59 292.628 1536.57 404.532Z"
+      stroke="url(#paint0_linear_118_3)"
+      stroke-width="110"
+    />
+    <path
+      d="M1536.57 404.532C1566.56 516.437 1526.19 643.275 1421.82 759.973C1317.83 876.237 1154.64 976.501 956.078 1029.71C757.513 1082.91 566.055 1077.68 417.87 1028.98C269.133 980.102 170.753 890.441 140.769 778.537C110.784 666.632 151.153 539.794 255.524 423.097C359.508 306.832 522.698 206.568 721.263 153.363C919.828 100.158 1111.29 105.393 1259.47 154.09C1408.21 202.967 1506.59 292.628 1536.57 404.532Z"
+      stroke="url(#pattern0)"
+      stroke-opacity="0.2"
+      stroke-width="110"
+    />
+  </g>
+  <defs>
+    <filter
+      id="filter0_ddii_118_3"
+      x="0.065918"
+      y="0.480957"
+      width="1685.21"
+      height="1222.11"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dy="4" />
+      <feGaussianBlur stdDeviation="5" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="BackgroundImageFix"
+        result="effect1_dropShadow_118_3"
+      />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dx="4" dy="20" />
+      <feGaussianBlur stdDeviation="40" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0.937255 0 0 0 0 0.615686 0 0 0 0 0.513726 0 0 0 0.2 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="effect1_dropShadow_118_3"
+        result="effect2_dropShadow_118_3"
+      />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="effect2_dropShadow_118_3"
+        result="shape"
+      />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dx="-32" />
+      <feGaussianBlur stdDeviation="20" />
+      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0.937255 0 0 0 0 0.615686 0 0 0 0 0.513726 0 0 0 0.9 0"
+      />
+      <feBlend mode="normal" in2="shape" result="effect3_innerShadow_118_3" />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dx="30" dy="20" />
+      <feGaussianBlur stdDeviation="25" />
+      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0.1125 0 0 0 0 0.0697917 0 0 0 0 0.05625 0 0 0 1 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="effect3_innerShadow_118_3"
+        result="effect4_innerShadow_118_3"
+      />
+    </filter>
+    <pattern
+      id="pattern0"
+      patternContentUnits="objectBoundingBox"
+      width="1.31701"
+      height="2.01326"
+    >
+      <use
+        xlink:href="#image0_118_3"
+        transform="scale(0.000643069 0.000983036)"
+      />
+    </pattern>
+    <linearGradient
+      id="paint0_linear_118_3"
+      x1="-20.1322"
+      y1="1243.04"
+      x2="-162.315"
+      y2="1499.71"
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop stop-color="#0539E0" />
+      <stop offset="1" stop-color="#030E31" />
+    </linearGradient>
+    <image
+      id="image0_118_3"
+      width="2048"
+      height="100%"
+      xlink:href="/images/noise.webp"
+    />
+  </defs>
+</svg>
