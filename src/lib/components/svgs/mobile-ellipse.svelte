813947cb20bcab564e71321d6fe81de6c9c14148
Siddhant Khare <siddhant@gitpod.io>
2023-05-15 15:00:53 +0000

    [Mobile] fix: Pixelated ring - Hero

---
+++ src/lib/components/svgs/mobile-ellipse.svelte
@@ -0,0 +1,145 @@
+<script lang="ts">
+  let clazz = "";
+  let stylez = "";
+  export { stylez as style };
+  export { clazz as class };
+</script>
+
+<svg
+  width="362"
+  height="428"
+  viewBox="0 0 362 428"
+  fill="none"
+  class={clazz}
+  style={stylez}
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+>
+  <title>CDE Universe ring line</title>
+  <g filter="url(#filter0_ddii_236_2099)">
+    <path
+      d="M1354.14 -1092.83C1645.14 -1205.61 1643.53 -901.466 1650.17 -635.822C1662.86 -128.527 632.687 473.859 269.146 206.47C-61.9417 -37.0487 265.525 -542.452 604.507 -690.137L946.737 -860.46L1354.14 -1092.83Z"
+      stroke="url(#paint0_linear_236_2099)"
+      stroke-width="110"
+      stroke-linecap="round"
+    />
+    <path
+      d="M1354.14 -1092.83C1645.14 -1205.61 1643.53 -901.466 1650.17 -635.822C1662.86 -128.527 632.687 473.859 269.146 206.47C-61.9417 -37.0487 265.525 -542.452 604.507 -690.137L946.737 -860.46L1354.14 -1092.83Z"
+      stroke="url(#pattern0)"
+      stroke-opacity="0.2"
+      stroke-width="110"
+      stroke-linecap="round"
+    />
+  </g>
+  <defs>
+    <filter
+      id="filter0_ddii_236_2099"
+      x="0.44873"
+      y="-1232"
+      width="1788.84"
+      height="1659.15"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dy="4" />
+      <feGaussianBlur stdDeviation="5" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="BackgroundImageFix"
+        result="effect1_dropShadow_236_2099"
+      />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dx="4" dy="20" />
+      <feGaussianBlur stdDeviation="40" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0.937255 0 0 0 0 0.615686 0 0 0 0 0.513726 0 0 0 0.2 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="effect1_dropShadow_236_2099"
+        result="effect2_dropShadow_236_2099"
+      />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="effect2_dropShadow_236_2099"
+        result="shape"
+      />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dx="-32" />
+      <feGaussianBlur stdDeviation="20" />
+      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0.937255 0 0 0 0 0.615686 0 0 0 0 0.513726 0 0 0 0.9 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="shape"
+        result="effect3_innerShadow_236_2099"
+      />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dx="30" dy="20" />
+      <feGaussianBlur stdDeviation="25" />
+      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0.1125 0 0 0 0 0.0697917 0 0 0 0 0.05625 0 0 0 1 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="effect3_innerShadow_236_2099"
+        result="effect4_innerShadow_236_2099"
+      />
+    </filter>
+    <pattern
+      id="pattern0"
+      patternContentUnits="objectBoundingBox"
+      width="1.15651"
+      height="2.12926"
+    >
+      <use
+        xlink:href="#image0_236_2099"
+        transform="scale(0.000564701 0.00103968)"
+      />
+    </pattern>
+    <linearGradient
+      id="paint0_linear_236_2099"
+      x1="1799.24"
+      y1="-1194.03"
+      x2="1870.76"
+      y2="-1486.29"
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop stop-color="#0539E0" />
+      <stop offset="1" stop-color="#030E31" />
+    </linearGradient>
+  </defs>
+</svg>
