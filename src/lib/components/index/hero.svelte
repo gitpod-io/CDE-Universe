62be6a85dcc9da9e5fad3d58d93ede04b5666dc6
Siddhant Khare <siddhant@gitpod.io>
2023-05-15 10:24:35 +0000

    :sparkles: CDE Universe Ellipse

--- src/lib/components/index/hero.svelte
+++ src/lib/components/index/hero.svelte
@@ -1,5 +1,6 @@
 <script>
   import PrimaryButton from "../library/primary-button.svelte";
+  import DesktopEllipse from "../svgs/ellipse.svelte";
 </script>
 
 <div
@@ -29,13 +30,9 @@
     </div>
   </div>
 
-  <img
-    src="/images/primary-ellipse.webp"
-    alt="CDE Ellipse"
-    draggable={false}
-    class="hidden md:flex max-w-4xl -z-10 select-none w-[85vw] transfrom scale-[2.12] absolute translate-x-[100%] translate-y-[-180%] rotate-[-6deg] md:transform md:translate-x-[25%] md:translate-y-[-95%] md:scale-[2.10]"
+  <DesktopEllipse
+    class="hidden md:flex max-w-4xl -z-10 select-none w-[85vw] transfrom scale-[2.12] absolute translate-x-[100%] translate-y-[-180%] rotate-[-10deg] md:transform md:translate-x-[25%] md:translate-y-[-75%] md:scale-[2.10]"
   />
-
   <img
     style="top: -20%; left: 30%; transform: translate(-20%, -50%)"
     class="flex md:hidden absolute w-screen min-w-[65vw] z-[-10] scale-150"
