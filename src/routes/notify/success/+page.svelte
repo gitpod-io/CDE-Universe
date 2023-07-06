5410403ee12c6fe3fefea728caf1d49f341fb637
Siddhant Khare <siddhant@gitpod.io>
2023-07-06 05:46:06 +0000

    Change sucess page copy text

--- src/routes/notify/success/+page.svelte
+++ src/routes/notify/success/+page.svelte
@@ -5,8 +5,8 @@
 <div class="flex flex-col gap-8 items-center text-center mt-32">
   <h1 class="text-5xl md:text-8xl font-normal">You're signed up</h1>
   <p class="text-md md:text-2xl max-w-md md:max-w-[546px]">
-    We'll get back to you shortly via email to let you know if you got one of
-    the limited tickets.
+    We'll email you when we have more information about next year's CDE
+    Universe.
   </p>
   <PrimaryButton
     href="https://www.gitpod.io/cde"
