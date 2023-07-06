0b5c8627387a6bab9ac84109cc9f2df40f9dbd73
Siddhant Khare <siddhant@gitpod.io>
2023-07-06 06:25:52 +0000

    update notify success page

--- src/routes/notify/success/+page.svelte
+++ src/routes/notify/success/+page.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
   import PrimaryButton from "$lib/components/library/primary-button.svelte";
+  import OpenGraph from "$lib/components/library/open-graph.svelte";
 </script>
 
-<div class="flex flex-col gap-8 items-center text-center mt-32">
-  <h1 class="text-5xl md:text-8xl font-normal">You're signed up</h1>
-  <p class="text-md md:text-2xl max-w-md md:max-w-[546px]">
-    We'll email you when we have more information about next year's CDE
-    Universe.
+<OpenGraph
+  data={{
+    title: "Successfully subscribed to CDE Universe",
+    description:
+      "You will hear from us once we have any updates on the next events. Make sure not to miss the next CDE universe.",
+  }}
+/>
+<div class="flex flex-col items-center mb-12 mx-auto">
+  <h1
+    class="font-bold md:font-extrabold text-[64px] leading-none mb-4 max-w-xl"
+  >
+    Get notified
+  </h1>
+  <p class="text-base md:text-lg !mx-auto max-w-xl">
+    Make sure not to miss the next CDE universe.
+  </p>
+</div>
+<div
+  class="rounded-[40px] border-rose border-[10px] px-8 md:px-14 py-14 max-w-2xl mx-auto"
+>
+  <h1 class="font-bold max-w-xl text-h4 leading-none mb-6 mx-auto">Done.</h1>
+  <p class="mb-14">
+    You will hear from us once we have any updates on the next events.
   </p>
   <PrimaryButton href="https://www.gitpod.io/cde" text="Lear more about CDEs" />
 </div>
